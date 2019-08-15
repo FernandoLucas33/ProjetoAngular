47514902af36776654fef369a5cd5fd960441def
Fernando Lucas <fernandolucassud@gmail.com>
2019-08-14 21:37:08 -0300

    Comite Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  lancamento =[
+  lancamento = [
     { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
       dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José' },
     { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/06/2017',
